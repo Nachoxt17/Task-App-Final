3d686a82d01c8928767559c98010258bd978e850
Ignacio Ceaglio <nachoceaglio17@gmail.com>
2020-06-04 20:36:09 -0300

    Updates

---
+++ precache-manifest.85596cf346c8b1bbab8a240dba7ed6e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "993daf8baf3285e302754272d5f5d538",
+    "url": "/Task-App-Final/index.html"
+  },
+  {
+    "revision": "1772dfa40b520bd76522",
+    "url": "/Task-App-Final/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "78c8a19339bc7e320955",
+    "url": "/Task-App-Final/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "1772dfa40b520bd76522",
+    "url": "/Task-App-Final/static/js/2.06944839.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Task-App-Final/static/js/2.06944839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78c8a19339bc7e320955",
+    "url": "/Task-App-Final/static/js/main.f3e02eee.chunk.js"
+  },
+  {
+    "revision": "4ce0097937ffffcbe9eb",
+    "url": "/Task-App-Final/static/js/runtime-main.c1d7a095.js"
+  }
+]);
